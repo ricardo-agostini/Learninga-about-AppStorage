9b74951f7689e2992abafcf1eb996f8a5d68ef1d
Ricardo de Aagostini <ricardoagneto@gmail.com>
2023-12-22 14:53:18 -0300

    first commit

---
+++ AppStorageTutorial/AppStorageTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageTutorialApp.swift
+//  AppStorageTutorial
+//
+//  Created by Ricardo de Agostini Neto on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStorageTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppStorageTutorial
+//
+//  Created by Ricardo de Agostini Neto on 22/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
